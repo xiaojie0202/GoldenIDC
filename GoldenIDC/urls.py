eb80042c335b6edbe894b1d28f40b01450708d03
XiaoJie <wang141807087@qq.com>
2018-04-20 23:53:49 +0800

    erro

--- GoldenIDC/urls.py
+++ GoldenIDC/urls.py
@@ -12,6 +12,7 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
 from golden_web import urls
